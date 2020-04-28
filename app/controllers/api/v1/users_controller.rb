c56bb9aaf037d2a8a3460d56f66e8a74ab2e9769
Mii4a <syoutacancer123>
2020-04-28 18:40:34 +0900

    modify users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,18 @@
-class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+# frozen_string_literal: true
+
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    render json: @users
   end
 
   # GET /users/1
   # GET /users/1.json
-  def show
-  end
+  def show; end
 
   # POST /users
   # POST /users.json
@@ -41,13 +43,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
