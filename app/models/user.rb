e9810325dc27a55cb28302c22fa16464398735ee
Mii4a <syoutacancer123>
2020-04-29 17:19:34 +0900

    Add test datas of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name
+  validates :email
+  validates :password
+  validates :password_confirmation
+  has_secure_token
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_165730) do
+ActiveRecord::Schema.define(version: 2020_04_29_075858) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_165730) do
     t.string "password_confirmation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end
