8911146da656ec56e81b7e8d8873aef0e7887c03
Mii4a <syoutacancer123>
2020-05-03 00:15:42 +0900

    Succeed to create users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,9 +21,7 @@ class Api::V1::UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     if @user.save
-
       payload = { user_id: @users.id }
       session = JWTSessions::Session.new(payload: payload,
                                          refresh_by_access_allowed: true)
@@ -34,7 +32,7 @@ class Api::V1::UsersController < ApplicationController
                           secure: Rails.env.production?)
       render json: { csrf: tokens[:csrf] }
     else
-      render json: { error: user.error.full_messages.join(' ') },
+      render json: { error: @user.error.full_messages.join(' ') },
              status: :unprocessable_entity
     end
   end
